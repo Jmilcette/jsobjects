1ae2600c37c16bbd10a9786119b0ac0d76b9c742
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-10-13 14:15:56 +0000

    created coinsObject file

---
+++ coinsObject.js
@@ -0,0 +1,11 @@
+var assignment = "create an object for coins";
+
+var currencyAbbr = ["AUD", "GBP", "EUR", "JPY", "CHF", "USD", "AFN", "ALL", "DZD", "AOA"];
+
+var USD = {
+    penny: .01,
+    nickel: .05,
+    quarter: .25,
+    halfDollar: .50,
+    dollarCoin: 1
+};
