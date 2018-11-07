bdc9c13522b8cf42a775a6549c6c102f648d23b8
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-11-07 00:07:30 +0000

    added rooms to the javascript file

---
+++ Hotel/hotels2.js
@@ -0,0 +1,43 @@
+var rooms = [];
+rooms[1] = {
+    roomNumber: 103,
+    name: "Bjorn's Palace",
+    amenities: {
+
+    }
+    price: "$600 a night",
+    
+}
+
+rooms[2] = {
+    roomNumber: 666,
+    name: "The Nightmare Chamber",
+    price:"$666 a night",
+    
+}
+
+rooms[3] = {
+    roomNumber: 100
+    name: "The Gold Suite",
+    price:"$100 a night",
+    
+    
+}
+
+rooms[4] = {
+    roomNumber: 102
+    name: "The Refuse Alcove",
+    price:"$1000 a night",
+    
+}
+
+function display() {
+    var chosenRoom = document.getElementById("chooser").value;
+    document.getElementById("displayroom").innerText = chosenRoom;
+}
+
+function occupyRoom() {
+    if () {
+
+    }
+}
