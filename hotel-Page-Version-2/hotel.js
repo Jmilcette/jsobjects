15290c7a1e1aa19b49bd5505f9a86e138a07934a
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-11-24 02:26:04 +0000

    minor changes

--- hotel-Page-Version-2/hotel.js
+++ hotel-Page-Version-2/hotel.js
@@ -40,6 +40,5 @@ var hotel = {
         this.roomNumbersBooked = this.roomNumbers.splice(this.roomNumbers.indexOf(randomRoom), 1).concat(this.roomNumbersBooked);
         console.log(this.roomNumbersBooked);
         return;
-        
     }
 };
