4dc077c1f7ac1c7ba76735ea8165df1b302bbfa2
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-01 17:22:20 +0000

    created javascript venue directory

---
+++ DOM2/OOP/venue/README.md
@@ -0,0 +1,7 @@
+Creating a venue program    
+-create html page
+    
+-create javascript page
+    -venue object
+    -methods
+        -
---
+++ DOM2/OOP/venue/venue.js
@@ -0,0 +1,18 @@
+var venue = {
+    name: "CareerDevs Venue",
+    ticketPrice: 250,
+    seatsAvailable: ["1","2","3","4","5","6"],
+    seatsBooked: [],
+    viewAvailableSeats: function() {
+        
+    },
+    purchaseTicket: function() {
+     
+    },
+    refundTicket: function() {
+        
+    },
+    
+};
+
+var seatsList = "<ul>";
